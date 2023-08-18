698f17f4d2271fdea907ba9dd4a0f10996a78739
Adriana Acosta <126767867+adriadc190715@users.noreply.github.com>
2023-08-18 13:02:24 +0200

    #decaraciones internas

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,62 @@
-const numTurno = (document.querySelector(".numero-turno"));
-const elegirTurno = document.getElementById('elegir-turno') ;
-
 let numInicio = 1;
 
-function actualizarTurno(){
-   
-   if (numTurno !== null && numTurno !== undefined ) {
-      numTurno.textContent = numInicio.toString().padStart(2, '0');
-     }
-   
+function actualizarTurno(numero: number) {
+   const numTurno = document.querySelector(".numero-turno") as HTMLHeadingElement;
+
+   if (numTurno) {
+      numTurno.textContent = numero.toString().padStart(2, '0');
+   }
 }
+
 function siguienteTurno() {
    numInicio++;
-   actualizarTurno();
+   actualizarTurno(numInicio);
 }
+
 function anteriorTurno() {
    if (numInicio >= 1) {
       numInicio--;
-    }
-   actualizarTurno();
- }
+   }
+   actualizarTurno(numInicio);
+}
 
 function resetTurno() {
    numInicio = 1;
-   actualizarTurno();
+   actualizarTurno(numInicio);
 }
 
-
-
 function elegirTurnos() {
-   if (elegirTurno !== null && elegirTurno !== undefined && elegirTurno instanceof HTMLInputElement){
+   const elegirTurno = document.getElementById('elegir-turno') as HTMLInputElement;
+
+   if (elegirTurno !== null && elegirTurno instanceof HTMLInputElement) {
       const turnoElegido = parseInt(elegirTurno.value, 10);
 
-   if (!isNaN(turnoElegido) && turnoElegido >= 1) {
-     numInicio = turnoElegido;
-     actualizarTurno();
+      if (!isNaN(turnoElegido) && turnoElegido >= 1) {
+         numInicio = turnoElegido;
+         actualizarTurno(numInicio);
+      }
    }
- }
- }
+}
 
-const btnTurnoSigu = document.getElementById('btnTurnoSigu');
-const btnTurnoAntr = document.getElementById('btnTurnoAntr');
-const btnReset = document.getElementById('btnReset');
-const btnElegir = document.getElementById('btnElegir');
+const btnTurnoSigu = document.getElementById('btnTurnoSigu') as HTMLButtonElement;
+const btnTurnoAntr = document.getElementById('btnTurnoAntr') as HTMLButtonElement;
+const btnReset = document.getElementById('btnReset') as HTMLButtonElement;
+const btnElegir = document.getElementById('btnElegir') as HTMLButtonElement;
 
-
-if (btnTurnoSigu!== null && btnTurnoSigu!== undefined && btnTurnoSigu instanceof HTMLButtonElement) {
+if (btnTurnoSigu) {
    btnTurnoSigu.addEventListener("click", siguienteTurno);
-   }
-if ( btnTurnoAntr!== null && btnTurnoAntr!== undefined && btnTurnoAntr instanceof HTMLButtonElement){
+}
+
+if (btnTurnoAntr) {
    btnTurnoAntr.addEventListener('click', anteriorTurno);
-   }   
-if (btnReset!== null && btnReset!== undefined && btnReset instanceof HTMLButtonElement){
+}
+
+if (btnReset) {
    btnReset.addEventListener('click', resetTurno);
-   }
-if (btnElegir!== null && btnElegir!== undefined && btnElegir instanceof HTMLButtonElement){
+}
+
+if (btnElegir) {
    btnElegir.addEventListener('click', elegirTurnos);
-   }
+}
 
-
+actualizarTurno(numInicio);
